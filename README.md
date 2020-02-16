8aa2338af32dbbf400415b67c2f73b7e456426bd
niki-01 <18622989+nikimanoledaki@users.noreply.github.com>
2020-02-16 11:47:54 +0000

    Adds basic user interface

--- README.md
+++ README.md
@@ -3,13 +3,22 @@
 ## Description
 The FizzBuzz kata, written and test-driven in Python. Learning Python independently with the skills I learned from Makers was a little week-end pet project of mine. I was inspired to learn how to TDD Python after I came across [this article](https://code.likeagirl.io/in-tests-we-trust-tdd-with-python-af69f47e6932) from the Code Like a Girl blog.
 
-## Testing instructions
+## Run it
+```
+~ python fizzbuzz.py
+Lets FizzBuzz-ify numbers!
+q: quit
+Pick a number, any number:
+```
+
+## Test it
 Ensure that you have [pytest](https://docs.pytest.org/en/latest/getting-started.html#getstarted) installed:
 ```
 pip install -U pytest
+pytest --version
 ```
 
-Then run it to test FizzBuzz:
+Then run pytest inside the folder to check that tests pass:
 ```
 pytest
 ```
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,5 @@
 import numbers
+import sys
 
 class FizzBuzzError(Exception):
     """An exception class for fizzbuzz."""
@@ -9,7 +10,7 @@ class FizzBuzz:
     def run(self, number):
         self._check_operand(number)
         result = []
-        x = range(1, number + 1)
+        x = range(start=1, end=number + 1)
         for n in x:
             if n % 3 == 0 and n % 5 == 0:
                 result.append('FizzBuzz')
@@ -25,3 +26,14 @@ class FizzBuzz:
     def _check_operand(self, operand):
         if not isinstance(operand, numbers.Number):
             raise FizzBuzzError(f'"{operand}" was not a number')
+
+if __name__ == '__main__':
+    print("Lets FizzBuzz-ify numbers!")
+    fizzbuzz = FizzBuzz() 
+    programme = [fizzbuzz.run]
+
+    while True:
+        print("q: quit")
+        programme = input("Pick a number, any number: ")
+        if programme == 'q':
+            sys.exit()
